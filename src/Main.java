91db765f8a0840c9d68e4a627da413edb48a0cbe
MariaSizz <buchacik@mail.ru>
2024-11-21 01:53:05 +0300

    DZ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,74 @@
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //1
+        int[] nums = new int[3];
+        for (int i = 0;i < nums.length;i++) {
+            nums[i] = i + 1;
+        }
+        double[] doubles = {1.57,7.654,9.9};
+        boolean[] bools = {false,false,true};
+
+        //2
+        for (int i = 0; i < nums.length; i++){
+            System.out.print(nums[i]);
+            if (i < nums.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < doubles.length; i++) {
+            System.out.print(doubles[i]);
+            if (i< doubles.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < bools.length; i++) {
+            System.out.print(bools[i]);
+            if (i< bools.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        //3
+        for (int i = nums.length-1; i >= 0; i--) {
+            System.out.print(nums[i]);
+            if (i>0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = bools.length-1; i >=0; i--) {
+            System.out.print(doubles[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = bools.length-1; i >= 0 ; i--) {
+            System.out.print(bools[i]);
+            if (i > 0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        System.out.println();
+
+        //4
+        for(int i = 0;i < nums.length; i++) {
+            if (nums[i] % 2 != 0) {
+                nums[i]++;
+            }
+        }
+        for(int i = 0;i < nums.length; i++) {
+            System.out.print(nums[i]);
+            if (i < nums.length - 1) {
+                System.out.print(", ");
+            }
+        }
     }
 }
